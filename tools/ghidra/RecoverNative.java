0ef14e77c01c77edbcaf708687884d3ad51fdee1
maiyuanoo <masiyuanyyds@qq.com>
2026-08-23 14:33:08 +0800

    feat: 导出相机状态应用调用

--- tools/ghidra/RecoverNative.java
+++ tools/ghidra/RecoverNative.java
@@ -22,7 +22,9 @@ public class RecoverNative extends GhidraScript {
         long entryOffset = function.getEntryPoint().getOffset();
         return entryOffset == 0x17098L || entryOffset == 0x17184L ||
             entryOffset == 0x17720L || entryOffset == 0x195a8L ||
-            entryOffset == 0x19cf0L || entryOffset == 0x19de8L;
+            entryOffset == 0x19cf0L || entryOffset == 0x19de8L ||
+            entryOffset == 0x1a0f4L || entryOffset == 0x1a4e8L ||
+            entryOffset == 0x1a5b0L || entryOffset == 0x1a730L;
     }
 
     @Override
